79b09a29d2591b01a6795a67676a3887b0c729c7
kamini <kaminigohatre05255@gmail.com>
2024-12-25 19:27:53 +0530

    diff lines

--- app.py
+++ app.py
@@ -7,6 +7,6 @@ response = client.run_instances(
     ImageId='ami-0614680123427b75e',
     InstanceType='t2.micro',
     KeyName='8DEC',
-    MaxCount=1,
-    MinCount=1
+    MaxCount=2,
+    MinCount=2
 )
